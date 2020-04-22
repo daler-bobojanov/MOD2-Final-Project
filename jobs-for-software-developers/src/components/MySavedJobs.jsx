53ffd63ae265b59f0df3d7cb33d2cde79c7121cc
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-04-22 11:23:25 -0400

    functional component

---
+++ jobs-for-software-developers/src/components/MySavedJobs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const MySavedJobs = () => {
+    return (
+        <div>
+            <h2>This is My Saved jobs tab</h2>
+        </div>
+    );
+}
+
+export default MySavedJobs;
