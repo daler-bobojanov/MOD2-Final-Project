18939c7f69572b279b6416742281f7513c1632c1
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-04-22 16:26:31 -0400

    modified

--- jobs-for-software-developers/src/components/AllJobs.jsx
+++ jobs-for-software-developers/src/components/AllJobs.jsx
@@ -1,12 +1,55 @@
 import React from 'react'
+import axios from 'axios';
 
+import '../styles/AllJobs.css';
+import SearchForm from './SearchForm';
+// const BASE_URL = 'https://jobs.github.com/positions.json?';
 
-const AllJobs = () => {
-    return (
-        <div>
-            <h2>This is All jobs tab</h2>
-        </div>
-    );
+class AllJobs extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        this.getAllJobs();
+    }
+
+    async getAllJobs() {
+        try {
+            const api_call = await axios.get(`https://jobs.github.com/positions.json?&page=`);
+            // console.log(api_call);
+            this.setState({ data: api_call.data })
+            console.log(this.state.data, 'This is result of this.state');
+            return api_call;
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    render() {
+        const postJobs = this.state.data.map(post => (
+            <div className="container-all-jobs" key={post.id}>
+                <a href={post.url}>{post.title}</a>
+                <br />
+                <p>Company: <a href={post.company_url} target="_blank">{post.company}</a></p>
+                <p>Location: {post.location}</p>
+                <p>Type: {post.type}</p>
+                <p>Posted on: {post.created_at}</p>
+            </div>
+        ))
+        return (
+            <React.Fragment>
+                <SearchForm />
+                <hr /><br />
+                {postJobs}
+
+                {/* <h2>This is All jobs tab</h2> */}
+            </React.Fragment >
+        );
+    }
 }
 
 export default AllJobs;
