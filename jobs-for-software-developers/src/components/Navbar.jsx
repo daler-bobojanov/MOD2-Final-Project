97c17998bf37ba59693c1428a4d7f4437c18466b
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-04-22 12:07:31 -0400

    some modification

--- jobs-for-software-developers/src/components/Navbar.jsx
+++ jobs-for-software-developers/src/components/Navbar.jsx
@@ -1,24 +1,38 @@
 import React from 'react'
 import '../styles/Navbar.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import AllJobs from './AllJobs';
 import MySavedJobs from './MySavedJobs';
 import SignOut from './SignOut';
 import UserName from './UserName';
+import Logo from './Logo';
 
 const Navbar = () => {
     return (
         <Router>
             <nav>
-                <h3>My pretty Logo</h3>
+                <Link className="link" to="/">My Pretty Logo</Link>
                 <ul className="nav-links">
-                    <li><AllJobs /></li>
-                    <li><MySavedJobs /></li>
-                    <li><SignOut /></li>
-                    <li><UserName /></li>
+                    <li>
+                        <Link className="link" to="/alljobs">All jobs</Link>
+                    </li>
+                    <li>
+                        <Link className="link" to="/mysavedjobs">My Saved jobs</Link>
+                    </li>
+                    <li>
+                        Sign Out
+                    </li>
+                    <li>
+                        User Name
+                    </li>
                 </ul>
             </nav>
+            <Switch>
+                <Route exact path="/" component={Logo} />
+                <Route exact path='/alljobs' component={AllJobs} />
+                <Route exact path='/mysavedjobs' component={MySavedJobs} />
+            </Switch>
         </Router>
     );
 }
