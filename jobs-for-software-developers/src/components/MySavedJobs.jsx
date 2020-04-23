e474d816f1e7cedda3917500a4a20546d633a378
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-04-23 00:15:13 -0400

    some modification

--- jobs-for-software-developers/src/components/MySavedJobs.jsx
+++ jobs-for-software-developers/src/components/MySavedJobs.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 
 
-const MySavedJobs = () => {
-    return (
-        <div>
-            <h2>This is My Saved jobs tab</h2>
-        </div>
-    );
+class MySavedJobs extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            savedJobs: []
+        }
+    }
+
+    getJobID = () => {
+        let idArray = [];
+        idArray.push(this.props.jobID);
+        this.setState({ savedJobs: idArray })
+        // console.log(idArray, 'My ID array');
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <button type="button" onClick={this.getJobID}>Bookmark</button>
+                {console.log(this.idArray)}
+                {/* <h2>This is My Saved jobs tab</h2> */}
+            </React.Fragment>
+        );
+    }
 }
 
 export default MySavedJobs;
