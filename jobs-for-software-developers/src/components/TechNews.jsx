32d689cfe2f8520af23a8a8e287dcb0222c57a38
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-04-24 12:41:29 -0400

    new component

---
+++ jobs-for-software-developers/src/components/TechNews.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import axios from 'axios';
+
+
+const _api_key = process.env.REACT_APP_NEWS_API_KEY;
+
+class TechNews extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default TechNews;
