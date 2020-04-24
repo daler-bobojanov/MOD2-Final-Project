2ff68687b1f9c5877b8729a53cf0fde82966d7ba
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-04-24 09:42:27 -0400

    modified code

--- jobs-for-software-developers/src/components/SearchForm.jsx
+++ jobs-for-software-developers/src/components/SearchForm.jsx
@@ -1,22 +1,37 @@
 import React from 'react';
 import '../styles/SearchForm.css';
 
-const SearchForm = () => {
-    return (
-        <form className="search-form">
-            <div>
-                <label>Job Description</label><br />
-                <input type="text" name="" placeholder="Complete later" />
-            </div>
+const SearchForm = props => (
+    // return (
+    <form className="search-form" onSubmit={props.searchJobs}>
+        <div>
+            <label htmlFor="jobDescription">Job Description</label><br />
+            <input className="search-input" type="text" name="jobDescription" placeholder="Filter by title, company, expertise, or programming language.." />
+        </div>
+
+        <div>
+            <label htmlFor="location">Location</label><br />
+            <input className="search-input" type="text" name="location" placeholder="Filter by city, state, zip code or country..." />
+        </div>
+
+        <div>
             <br />
-            <div>
-                <label>Location</label><br />
-                <input type="text" name="" placeholder="Complete later" />
-            </div>
-            <br />
-            <button>Search</button>
-        </form>
-    );
-}
+            <input type="checkbox" name="fullTime" />
+            <label id="ft-only" htmlFor="fullTimeOnly">Full Time Only</label>
+        </div>
+        <button id="search-form-button" >Search</button>
+    </form>
+    // );
+    // }
+);
 
 export default SearchForm;
+
+/*
+TO DO:
+- make use of Full Time checkbox;
+- add image as placeholder;
+- fix button style;
+- make overall look prettier;
+*/
+
