00af1c95bf8cd57c0f80ab205a19bf0f365ce436
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-04-24 09:42:08 -0400

    modified code

--- jobs-for-software-developers/src/components/Navbar.jsx
+++ jobs-for-software-developers/src/components/Navbar.jsx
@@ -6,42 +6,49 @@ import homeBtn from '../assets/homeBtn.png';
 
 import AllJobs from './AllJobs';
 import MySavedJobs from './MySavedJobs';
-import SignOut from './SignOut';
-import UserName from './UserName';
+// import SignOut from './SignOut';
+// import UserName from './UserName';
 import Logo from './Logo';
-import SearchForm from './SearchForm';
+// import SearchForm from './SearchForm';
 
 
-const Navbar = (props) => {
-    return (
-        <Router>
-            <nav>
+class Navbar extends React.Component {
 
-                <ul className="nav-links">
-                    <li>
-                        <Link className="link" to="/"><img src={homeBtn} id='nav-img' alt="home-button" /></Link>
+    // function to Refresh the page upon Home button click
+    refreshPage() {
+        window.location.reload(false);
+    }
+
+    render() {
+        return (
+            <Router>
+                <nav>
+                    <ul className="nav-links">
+                        <li onClick={this.refreshPage}>
+                            <Link className="link" to="/"><img src={homeBtn} id='nav-img' alt="home-button" /></Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/alljobs">All jobs</Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/mysavedjobs">My Saved jobs</Link>
+                        </li>
+                        <li>
+                            Sign Out
                     </li>
-                    <li>
-                        <Link className="link" to="/alljobs">All jobs</Link>
+                        <li>
+                            User Name
                     </li>
-                    <li>
-                        <Link className="link" to="/mysavedjobs">My Saved jobs</Link>
-                    </li>
-                    <li>
-                        Sign Out
-                    </li>
-                    <li>
-                        User Name
-                    </li>
-                </ul>
-            </nav>
-            <Switch>
-                <Route exact path="/" component={Logo} />
-                <Route exact path='/alljobs' component={AllJobs} />
-                <Route exact path='/mysavedjobs' component={MySavedJobs} />
-            </Switch>
-        </Router>
-    );
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route exact path="/" component={Logo} />
+                    <Route exact path='/alljobs' component={AllJobs} />
+                    <Route exact path='/mysavedjobs' component={MySavedJobs} />
+                </Switch>
+            </Router>
+        );
+    }
 }
 
 export default Navbar;
