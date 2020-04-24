279959a9851766ca0c20e9a7c1d8f2d9eace5867
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-04-24 13:38:48 -0400

    rendered result of api call to dom

--- jobs-for-software-developers/src/components/TechNews.jsx
+++ jobs-for-software-developers/src/components/TechNews.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import axios from 'axios';
-
+import '../styles/TechNews.css';
 
 const _api_key = process.env.REACT_APP_NEWS_API_KEY;
 
 class TechNews extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            data: []
+        }
     }
 
     componentDidMount() {
@@ -18,7 +20,8 @@ class TechNews extends React.Component {
     async fetchNews() {
         try {
             const api_call = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=${_api_key}`);
-            console.log(api_call.data, "TEST");
+            console.log(api_call.data.articles);
+            this.setState({ data: api_call.data.articles });
         } catch (e) {
             console.error(e);
         }
@@ -26,10 +29,17 @@ class TechNews extends React.Component {
 
 
     render() {
-        return (
-            <div>
-
+        const postNews = this.state.data.map((post, id) => (
+            <div key={id} className="news-grid-container">
+                <h2>{post.title}</h2>
+                <p>{post.source.name}</p>
             </div>
+        ))
+        return (
+            <React.Fragment>
+                {postNews}
+
+            </React.Fragment>
         );
     }
 }
