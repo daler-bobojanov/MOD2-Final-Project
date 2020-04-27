263a76577cb7de3ab60d631d13753bfccb0f8970
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-04-27 19:26:21 -0400

    bootstrap css

--- jobs-for-software-developers/src/components/SearchForm.jsx
+++ jobs-for-software-developers/src/components/SearchForm.jsx
@@ -3,23 +3,29 @@ import '../styles/SearchForm.css';
 
 const SearchForm = props => (
     // return (
+    // <form className="search-form" onSubmit={props.searchJobs}>
     <form className="search-form" onSubmit={props.searchJobs}>
-        <div>
+        <div className="form-group">
             <label htmlFor="jobDescription">Job Description</label><br />
-            <input className="search-input" type="text" name="jobDescription" placeholder="Filter by title, company, expertise, or programming language.." />
+            {/* <input className="search-input" type="text" name="jobDescription" placeholder="Filter by title, company, expertise, or programming language.." /> */}
+            <input className="form-control" id="input-search1" type="text" name="jobDescription" placeholder="Filter by title, company, expertise, or programming language.." />
+
         </div>
 
-        <div>
+        <div className="form-group">
             <label htmlFor="location">Location</label><br />
-            <input className="search-input" type="text" name="location" placeholder="Filter by city, state, zip code or country..." />
+            {/* <input className="search-input" type="text" name="location" placeholder="Filter by city, state, zip code or country..." /> */}
+            <input className="form-control" type="text" name="location" placeholder="Filter by city, state, zip code or country..." />
+
         </div>
 
-        <div>
+        <div className="form-check ">
             <br />
-            <input type="checkbox" name="fullTime" />
-            <label id="ft-only" htmlFor="fullTimeOnly">Full Time Only</label>
+            <input type="checkbox" name="fullTime" className="form-check-input" />
+            <label htmlFor="fullTimeOnly" className="form-check-label">Full Time Only</label>
         </div>
-        <button id="search-form-button" >Search</button>
+        <button className="btn btn-primary btn-lg">Search</button>
+        {/* <button id="search-form-button" >Search</button> */}
     </form>
     // );
     // }
