1e641b403c94e3c2de7cee4db1cbf8259ab3ddd8
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-04-28 13:26:05 -0400

    code cleanup + comments added

--- jobs-for-software-developers/src/components/SearchForm.jsx
+++ jobs-for-software-developers/src/components/SearchForm.jsx
@@ -2,21 +2,17 @@ import React from 'react';
 import '../styles/SearchForm.css';
 
 const SearchForm = props => (
-    // return (
-    // <form className="search-form" onSubmit={props.searchJobs}>
+    // bootstrap library used to style form & elements
     <form className="search-form" onSubmit={props.searchJobs}>
+
         <div className="form-group">
             <label htmlFor="jobDescription">Job Description</label><br />
-            {/* <input className="search-input" type="text" name="jobDescription" placeholder="Filter by title, company, expertise, or programming language.." /> */}
             <input className="form-control" id="input-search1" type="text" name="jobDescription" placeholder="Filter by title, company, expertise, or programming language.." />
-
         </div>
 
         <div className="form-group">
             <label htmlFor="location">Location</label><br />
-            {/* <input className="search-input" type="text" name="location" placeholder="Filter by city, state, zip code or country..." /> */}
             <input className="form-control" type="text" name="location" id="input-search2" placeholder="Filter by city, state, zip code or country..." />
-
         </div>
 
         <div className="form-check ">
@@ -25,10 +21,8 @@ const SearchForm = props => (
             <label htmlFor="fullTimeOnly" className="form-check-label">Full Time Only</label>
         </div>
         <button className="btn btn-primary btn-lg">Search</button>
-        {/* <button id="search-form-button" >Search</button> */}
+
     </form>
-    // );
-    // }
 );
 
 export default SearchForm;
