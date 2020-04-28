bca68e246bdc3273f1b4a148e086bc17bf00b756
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-04-28 09:27:01 -0400

    code cleanup

--- jobs-for-software-developers/src/App.js
+++ jobs-for-software-developers/src/App.js
@@ -1,29 +1,13 @@
 import React from 'react';
-// import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
-// import SearchForm from './components/SearchForm';
 
-import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
-
-  async
-  render() {
-    return (
-      <div className="App">
-        {/* <header className="App-header"> */}
-        <Navbar />
-
-        {/* </header> */}
-        {/* <SearchForm /> */}
-
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
