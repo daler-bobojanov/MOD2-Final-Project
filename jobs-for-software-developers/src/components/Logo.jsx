1639d29af6a12aae69defefc841648c72a9e8e49
DALER BOBOJANOV <daler_bobojanov@DALERs-MacBook-Pro.local>
2020-04-28 09:21:37 -0400

    modified

--- jobs-for-software-developers/src/components/Logo.jsx
+++ jobs-for-software-developers/src/components/Logo.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/AllJobs.css';
 import SearchForm from './SearchForm';
 import JobDescriptionModal from './JobDescriptionModal';
